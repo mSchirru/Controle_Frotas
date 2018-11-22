41d61ae97c92b0cf4d9796ee8bed38790d2ccb0c
Schirru <schirru@MacBook-Pro-de-Mikael.local>
2018-11-22 15:45:12 -0200

    finished making associations

--- Menu.py
+++ Menu.py
@@ -1,6 +1,8 @@
 import ItemEntrega
 import Local
 import Caminhao
+import Pilha
+import Fila
 
 
 itens = []
@@ -8,13 +10,13 @@ locais = []
 caminhoes = []
 
 def main():
-    choice = '0'
-    while choice != '5':
+    choice = '-'
+    while choice != '0':
         print("\n **************************")
         print("MENU")
         print("[1] Pontos de Entrega")
-        print("[2] Inserir item de entrega;")
-        print("[3] Inserir caminhão")
+        print("[2] Itens de Entrega")
+        print("[3] Caminhões")
         print("[4] Associar item a ponto de entrega")
         print("[5] Associar ponto de entrega a caminhão")
         print("[6] Realizar entregas")
@@ -23,24 +25,16 @@ def main():
         choice = input ("Please make a choice: ")
 
         if choice == "5":
-            print("Go to another menu")
+            associar_local_caminhao()
+
         elif choice == "4":
-            print("Do Something 4")
+            associar_item_ptentrega()
+
         elif choice == "3":
-            novo_caminhao = Caminhao.criar_caminhao()
-            if any(caminhao.placa == novo_caminhao.placa for caminhao in caminhoes):
-                print("Esta placa já está em uso!")
-            else:
-                caminhoes.append(novo_caminhao)
-            print("Lista de caminhoes:", [caminhao.placa for caminhao in caminhoes])
+            menu_Caminhoes()
 
         elif choice == "2":
-            novo_item = ItemEntrega.criar_item()
-            if any(item.identificador == novo_item.identificador for item in itens):
-                print("Este identificador já está em uso!")
-            else:
-                itens.append(novo_item)
-            print("Lista de itens:", [item.nome for item in itens])
+            menu_ItensEntrega()
 
         elif choice == "1":
             menu_pontoEntrega()
@@ -100,8 +94,163 @@ def menu_pontoEntrega():
             else:
                 print("Local: {} | Indentificador: {} | Itens: {}".format(locais[index_detalhes].nome,
                                                                           locais[index_detalhes].identificador,
-                                                                          [item for item in locais[index_detalhes]
+                                                                          [item.nome for item in locais[index_detalhes]
                                                                           .itens]))
+
+def menu_ItensEntrega():
+    choice = '-'
+    while choice != '0':
+        print("[1] Inserir item de entrega")
+        print("[2] Remover item de entrega")
+        print("[3] Verificar os itens de entrega")
+        print("[0] Sair")
+
+        choice = input("Please make a choice: ")
+
+        if choice == "1":
+            novo_item = ItemEntrega.criar_item()
+            if any(item.identificador == novo_item.identificador for item in itens):
+                print("Este identificador já está em uso!")
+            else:
+                itens.append(novo_item)
+                print("{} adicionado com sucesso a lista de itens!".format(novo_item.nome))
+
+
+
+        elif choice == "2":
+            print("---------- Lista de Itens: ----------")
+            aux = 0
+            for item in itens:
+                print("| [{}] Item: {} | Identificador: {} |".format(aux, item.nome, item.identificador))
+                aux += 1
+            print("--------------------------------------")
+
+            index_deletar = int(input("Insira o numero do item a ser deletado: "))
+            print("Tem certeza que deseja deletar {} da sua lista de itens?".format(itens[index_deletar].nome))
+            escolha_deletar = input("[0] Confirmar\n[1] Cancelar")
+            if escolha_deletar == "0":
+                itens.pop(index_deletar)
+
+        elif choice == "3":
+            print("---------- Lista de Itens: ----------")
+            aux = 0
+            for item in itens:
+                print("| [{}] Item: {} | Identificador: {} |".format(aux, item.nome, item.identificador))
+                aux += 1
+            print("--------------------------------------")
+
+def menu_Caminhoes():
+    choice = '-'
+    while choice != '0':
+        print("[1] Inserir caminhão")
+        print("[2] Remover caminhão")
+        print("[3] Verificar detalhes de um caminhão")
+        print("[0] Sair")
+
+        choice = input("Please make a choice: ")
+
+        if choice == "1":
+            novo_caminhao = Caminhao.criar_caminhao()
+            if any(caminhao.placa == novo_caminhao.placa for caminhao in caminhoes):
+                print("Esta placa já está em uso!")
+            else:
+                caminhoes.append(novo_caminhao)
+            print("Lista de caminhoes:", [caminhao.placa for caminhao in caminhoes])
+
+
+
+        elif choice == "2":
+            print("---------- Lista de Caminhões: ----------")
+            aux = 0
+            for caminhao in caminhoes:
+                print("| [{}] Placa: {}".format(aux, caminhao.placa))
+                aux += 1
+            print("--------------------------------------")
+
+            index_deletar = int(input("Insira o numero do caminhao a ser deletado: "))
+            print("Tem certeza que deseja deletar o caminhao de placa {} da sua lista?".format(itens[index_deletar].nome))
+            escolha_deletar = input("[0] Confirmar\n[1] Cancelar")
+            if escolha_deletar == "0":
+                caminhoes.pop(index_deletar)
+
+        elif choice == "3":
+            print("---------- Lista de Caminhões: ----------")
+            aux = 0
+            for caminhao in caminhoes:
+                print("| [{}] Placa: {} ".format(aux, caminhao.placa))
+                aux += 1
+            print("--------------------------------------")
+
+            index_detalhes = int(input("Insira o numero do caminhão a ser detalhado: "))
+
+            if len(caminhoes[index_detalhes].conjuntoItens) == 0 and len(caminhoes[index_detalhes].conjuntoLocais) == 0:
+                print("Placa: {} | {}".format(caminhoes[index_detalhes].placa, "Fora de circulação"))
+            else:
+                print("Placa: {} | Locais: {} {}".format(caminhoes[index_detalhes].placa,
+                                                                          [local.nome for local in caminhoes[index_detalhes].conjuntoLocais],
+                                                         [[item.nome for item in local.itens1] for local in
+                                                          caminhoes[index_detalhes].conjuntoLocais]))
+
+def associar_item_ptentrega():
+    print("---------- Lista de Locais: ----------")
+    aux = 0
+    for local in locais:
+        print("| [{}] Local: {} | Identificador: {} |".format(aux, local.nome, local.identificador))
+        aux += 1
+    print("--------------------------------------")
+
+    escolha_local = int(input("Selecione primeiramente o local de entrega: "))
+
+    print("---------- Lista de Itens: ----------")
+    aux = 0
+    for item in itens:
+        print("| [{}] Item: {} | Identificador: {} |".format(aux, item.nome, item.identificador))
+        aux += 1
+    print("--------------------------------------")
+
+    choice = '-'
+    while choice != '1':
+        escolha_item = int(input("Selecione o item de entrega: "))
+        locais[escolha_local].itens.append(itens[escolha_item])
+        print("Item: {} adicionado ao Local: {}".format(itens[escolha_item].nome, locais[escolha_local].nome))
+        choice = input("[0] Adicionar novo item | [1] Sair")
+
+def associar_local_caminhao():
+    print("---------- Lista de Caminhões: ----------")
+    aux = 0
+    for caminhao in caminhoes:
+        print("| [{}] Placa: {}".format(aux, caminhao.placa))
+        aux += 1
+    print("--------------------------------------")
+
+    escolha_caminhao = int(input("Selecione primeiramente o caminhao de entrega: "))
+
+    print("---------- Lista de Locais: ----------")
+    aux = 0
+    for local in locais:
+        print("| [{}] Local: {} | Identificador: {} |".format(aux, local.nome, local.identificador))
+        aux += 1
+    print("--------------------------------------")
+
+    choice = '-'
+    while choice != '1':
+        escolha_local = int(input("Selecione o local de entrega: "))
+        caminhoes[escolha_caminhao].conjuntoLocais.append(locais[escolha_local])
+        print("Local: {} adicionado ao Caminhao: {}".format(locais[escolha_local].nome,
+                                                            caminhoes[escolha_caminhao].placa))
+
+        choice = input("[0] Adicionar novo item | [1] Sair")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
